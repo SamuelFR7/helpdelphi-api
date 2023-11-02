1972b546d632e8153d3f05358814878663df3f72
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-02 16:21:04 -0300

    feat: update actions and list

--- src/application/tickets/use-cases/update-ticket-use-case.ts
+++ src/application/tickets/use-cases/update-ticket-use-case.ts
@@ -1,12 +1,12 @@
 import { UseCaseError } from '@/application/errors/use-case-error'
-import { type ticketSchema } from '@/core/validations/ticket'
+import { type updateTicketSchema } from '@/core/validations/ticket'
 import { db } from '@/infra/db'
-import { tickets } from '@/infra/db/schema'
+import { actions, tickets } from '@/infra/db/schema'
 import { eq } from 'drizzle-orm'
 import { type z } from 'zod'
 
 export async function updateTicketUseCase(
-  input: z.infer<typeof ticketSchema> & {
+  input: z.infer<typeof updateTicketSchema> & {
     id: string
   }
 ): Promise<void> {
@@ -21,7 +21,25 @@ export async function updateTicketUseCase(
     throw new UseCaseError('Ticket not found')
   }
 
-  await db.update(tickets).set(input).where(eq(tickets.id, input.id))
+  await db.transaction(async (tx) => {
+    await tx
+      .update(tickets)
+      .set({
+        status: input.status,
+        criticality: input.criticality,
+      })
+      .where(eq(tickets.id, input.id))
+    if (input.actions.length > 0) {
+      await tx.insert(actions).values(
+        input.actions.flatMap((action) => {
+          return {
+            description: action.description,
+            ticketId: input.id,
+          }
+        })
+      )
+    }
+  })
 
   return
 }
--- src/infra/db/index.ts
+++ src/infra/db/index.ts
@@ -1,7 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { env } from '@/env.mjs'
+import * as schema from './schema'
 
 const connection = postgres(env.DATABASE_URL)
 
-export const db = drizzle(connection)
+export const db = drizzle(connection, { schema })
