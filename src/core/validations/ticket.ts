e77a2ad841a8d004ed2476fc6b5ac97cd9861998
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-10 20:49:32 -0300

    fix: create ticket with auth user id

--- src/core/validations/ticket.ts
+++ src/core/validations/ticket.ts
@@ -7,6 +7,12 @@ export const ticketSchema = z.object({
   status: z.enum(['waiting', 'in_progress', 'stopped', 'finished']),
 })
 
+export const ticketSchemaHttp = z.object({
+  subject: z.string().toUpperCase(),
+  criticality: z.number().min(1).max(3),
+  status: z.enum(['waiting', 'in_progress', 'stopped', 'finished']),
+})
+
 export const updateTicketSchema = z.object({
   criticality: z.number().min(1).max(3),
   status: z.enum(['waiting', 'in_progress', 'stopped', 'finished']),
--- src/infra/http/controllers/tickets/create-ticket.ts
+++ src/infra/http/controllers/tickets/create-ticket.ts
@@ -1,14 +1,19 @@
 import { createTicketUseCase } from '@/application/tickets/use-cases/create-ticket-use-case'
-import { ticketSchema } from '@/core/validations/ticket'
+import { ticketSchemaHttp } from '@/core/validations/ticket'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function createTicketController(
   req: FastifyRequest,
   res: FastifyReply
 ) {
-  const body = ticketSchema.parse(req.body)
+  const { sub } = req.user
 
-  await createTicketUseCase(body)
+  const body = ticketSchemaHttp.parse(req.body)
+
+  await createTicketUseCase({
+    clientId: sub,
+    ...body,
+  })
 
   return res.status(201).send({
     message: 'Ticket created',
