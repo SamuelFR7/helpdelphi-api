9702d043266c5e6c1cf8c160ce7dda7ff42e16d4
Samuel Ferreira Rezende <samuelferreirarezende@gmail.com>
2023-11-27 19:04:45 -0300

    fix: min size validation description and subject

--- src/core/validations/ticket.ts
+++ src/core/validations/ticket.ts
@@ -2,16 +2,16 @@ import { z } from 'zod'
 
 export const ticketSchema = z.object({
   clientId: z.string(),
-  subject: z.string().toUpperCase(),
-  description: z.string().toUpperCase(),
+  subject: z.string().min(1).toUpperCase(),
+  description: z.string().min(1).toUpperCase(),
   criticality: z.number().min(1).max(3),
   status: z.enum(['waiting', 'in_progress', 'stopped', 'finished']),
 })
 
 export const ticketSchemaHttp = z.object({
-  subject: z.string().toUpperCase(),
+  subject: z.string().min(1).toUpperCase(),
   criticality: z.number().min(1).max(3),
-  description: z.string().toUpperCase(),
+  description: z.string().min(1).toUpperCase(),
   status: z.enum(['waiting', 'in_progress', 'stopped', 'finished']),
 })
 
